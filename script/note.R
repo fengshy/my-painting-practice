0c86617288f0a247055dbf0b2e0c678823369df4
Feng Shuyun <1044532758@qq.com>
2020-12-01 22:40:45 +0800

    Initial commit

---
+++ script/note.R
@@ -0,0 +1,10 @@
+#header = FALSE 首行没有列名
+#name()为列命名
+#sep 设置分隔符
+#bar chart
+#stat = 'identity' 提取横坐标x对应y值  stat = ‘bin’表示x对应落到该x的样本数
+#在条形图中，position = ‘dodge’，是将多变量条形图不叠加
+#在条形图中，可以用colour给条形图边框填充颜色
+#rank函数是按从小到大排列
+#reorder() 按照条形图的高度进行排序
+#subset 获取子集
