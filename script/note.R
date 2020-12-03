e89fde1fd31073132cf31ca3a94541a77dda3066
Feng Shuyun <1044532758@qq.com>
2020-12-03 11:18:02 +0800

    Initial commit

--- script/note.R
+++ script/note.R
@@ -8,3 +8,28 @@
 #rank函数是按从小到大排列
 #reorder() 按照条形图的高度进行排序
 #subset 获取子集
+#width 调整条形图的条的宽度
+#position_dodge(0.9)调整多变量两个条形图之间的宽度，默认值是0.9
+#desc 将向量转换为降序排序
+#geom_text(aes(),vjust) 给条形图添加数据标签
+#vjust 竖直调整数据标签位置
+#arrange(data, first, second)用于排序，首先考虑first，其次按second
+#cumsum() 前n项和
+#ddply(data, variable, transform, newnol = function(data)) 给data新增一列，就最后
+#panel.grid.major.y = element_blank() 删除水平网格线
+#legend.position = c(1,0.5) 调整图例的位置
+#------------------------------------------------我是条形图和折线图的分隔线
+#ylim(low, high) 调整y轴的范围
+#scale_y_log10() 取对数
+#scale_colour_brewer(palette = '') 调用调色板
+#position_dodge(0.2) 错开位置
+#area chart use geom_chart
+
+
+
+
+
+
+
+
+
